39c75125300df81ae049f4c86b785e8efe712091
Parthiv <parthiv.varma@mail.utoronto.ca>
2023-11-11 17:29:22 -0500

    Fixed spacing for part 1

--- part1.py
+++ part1.py
@@ -1,18 +1,16 @@
 """
-Part 1: Graph Construction
+Graph Construction
 
-This part takes an input of intersections, and organizes it in an adjacency matrix fashion
+This part takes an input of intersections, and organizes it an adjacency matrix fashion
 """
 
 def remove_spaces(input_string):
     return input_string.replace(" ", "")
 
 def adjacency_list_to_matrix(adj_list):
-    adj_list = adj_list.split(',') # Obtain the individual node pairs
+    adj_list = adj_list.split(',')
     intersections = []
     intersection_pairs = []
-    
-    # Extract the intersections and the intersection pairs  
     for intersection_pair in adj_list:
         intersection_pair = intersection_pair.split("->")
         if len(intersection_pair) == 2:
@@ -22,17 +20,13 @@ def adjacency_list_to_matrix(adj_list):
     
     intersections = set(intersections) # Remove duplicates
     matrix = {}
-    
-    # Create an empty 2D matrix
     for intersection in intersections:
         matrix[intersection] = {}
-        
-    # Fill all the matrix values with infinity
+
     for intersection_i in intersections:
         for intersection_j in intersections:
             matrix[intersection_i][intersection_j] = float('inf')
     
-    # Fill in the cells with '1' for the 'from' and 'to' nodes in the graph 
     for intersection_pair in intersection_pairs: 
         matrix[intersection_pair[0].strip()][intersection_pair[1].strip()] = 1
     
@@ -42,15 +36,27 @@ def display_adjacency_matrix(adjacency_matrix):
     headers = list(adjacency_matrix.keys())
     max_header_length = max(len(header) for header in headers)
 
+    # Find max_number_length
+    max_number_length = max(
+        max(len(str(value)) for value in row.values()) for row in adjacency_matrix.values()
+    )
+
+    # Determine the overall maximum length between headers and numbers
+    max_combined_length = max(max_header_length, max_number_length)
+
     # Print headers
-    print(f"{' ' * (max_header_length + 2)}|", end="")
+    print(f"{' ' * (max_combined_length + 1)}|", end="")
     for header in headers:
-        print(f" {header} |", end="")
-    print("\n" + "-" * (max_header_length + 2) + "+---" * len(headers))
+        print(f" {header.rjust(max_combined_length)} |", end="")
+    print("\n" + "-" * (max_combined_length + 2) + "+---" * len(headers))
 
     # Print matrix
     for row in headers:
-        print(f"{row} {' ' * (max_header_length - len(row))} |", end="")
+        print(f"{row.ljust(max_combined_length)} |", end="")
         for col in headers:
-            print(f" {adjacency_matrix[row][col]} |", end="")
+            print(f" {adjacency_matrix[row][col]:>{max_combined_length}} |", end="")
         print()
+
+
+# matrix = adjacency_list_to_matrix("a->b, b->c, c->d, d->b") #Row is source, Col is target
+# display_adjacency_matrix(matrix)
