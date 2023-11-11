0f06e8d1a96ba38bb7f4c49ed00f4b5c3a0a9933
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-11-11 17:33:15 -0500

    Comment out part3.py

--- part3.py
+++ part3.py
@@ -45,6 +45,7 @@ def parse_input_string_part3(matrix_cost, matrix_time, input_string):
 
     for edge in edges_with_cost:
         
+        # Find source node, target node, cost and cooldown time
         source, target_with_cost = edge.split("->")
         target, cost_with_cooldown = target_with_cost.split("($")
         cost, cooldown = cost_with_cooldown.split(",")
@@ -56,50 +57,23 @@ def parse_input_string_part3(matrix_cost, matrix_time, input_string):
         
         assert matrix_cost[source][target] == 1
         
+        # Update matrix_cost and matrix_cost -- both are 2d dicts holding cost and cooldown values respectively
         matrix_cost[source][target] = cost
         matrix_time[source][target] = cooldown
 
-def dijkstra_algorithm(matrix, source):
-    num_intersections = len(matrix)
-
-    lowest_costs = {}
-    for inter in matrix.keys():
-        lowest_costs[inter] = float('inf')
-    lowest_costs[source] = 0
-
-    visited_inter = {}
-    for inter in matrix.keys():
-        visited_inter[inter] = False
-
-    best_path = {}
-
-    for _ in range(num_intersections - 1):
-        curr_source = -1
-
-        for inter in matrix.keys():
-            if not visited_inter[inter]:
-                if (curr_source == -1 or lowest_costs[inter] < lowest_costs[curr_source]):
-                    curr_source = inter
-
-        visited_inter[curr_source] = True
-
-        for curr_target in matrix.keys():
-            if matrix[curr_source][curr_target] != 0 and not visited_inter[curr_target]:
-                new_distance = lowest_costs[curr_source] + matrix[curr_source][curr_target]
-                if new_distance < lowest_costs[curr_target]:
-                    lowest_costs[curr_target] = min(lowest_costs[curr_target], new_distance)
-                    best_path[curr_target] = curr_source
-                
-    return lowest_costs, best_path
-
 def find_optimal_path_part3(start_inter, end_inter, max_time, input_string):
+    # Remove everything but the intersection pair from the string
     filtered_str = re.sub(r'\(\$[0-9]+, [0-9]+ min cooldown\)', '', input_string)
+
+    # Unweighted adjacency matrices
     matrix_cost = adjacency_list_to_matrix(filtered_str)
+    # Deepcopy so modifying one matrix doesn't impact the other
     matrix_time = copy.deepcopy(matrix_cost)
 
-
+    # Update the above two matrices to be weighted
     parse_input_string_part3(matrix_cost, matrix_time, input_string)
 
+    # Find the best possible cost and path given start intersection, end intersection and max time
     best_path, best_cost = tsp_part3(matrix_cost, matrix_time, start_inter, end_inter, max_time)
     return best_cost, best_path
 
