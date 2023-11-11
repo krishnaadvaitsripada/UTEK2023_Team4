9d1d14601e30b43f30e8bf69a84868393898ff54
Krishna Advait Sripada <sripada.advait@gmail.com>
2023-11-11 17:22:31 -0500

    Added comments for part1.py

--- part1.py
+++ part1.py
@@ -1,16 +1,18 @@
 """
-Graph Construction
+Part 1: Graph Construction
 
-This part takes an input of intersections, and organizes it an adjacency matrix fashion
+This part takes an input of intersections, and organizes it in an adjacency matrix fashion
 """
 
 def remove_spaces(input_string):
     return input_string.replace(" ", "")
 
 def adjacency_list_to_matrix(adj_list):
-    adj_list = adj_list.split(',')
+    adj_list = adj_list.split(',') # Obtain the individual node pairs
     intersections = []
     intersection_pairs = []
+    
+    # Extract the intersections and the intersection pairs  
     for intersection_pair in adj_list:
         intersection_pair = intersection_pair.split("->")
         if len(intersection_pair) == 2:
@@ -20,13 +22,17 @@ def adjacency_list_to_matrix(adj_list):
     
     intersections = set(intersections) # Remove duplicates
     matrix = {}
+    
+    # Create an empty 2D matrix
     for intersection in intersections:
         matrix[intersection] = {}
-
+        
+    # Fill all the matrix values with infinity
     for intersection_i in intersections:
         for intersection_j in intersections:
             matrix[intersection_i][intersection_j] = float('inf')
     
+    # Fill in the cells with '1' for the 'from' and 'to' nodes in the graph 
     for intersection_pair in intersection_pairs: 
         matrix[intersection_pair[0].strip()][intersection_pair[1].strip()] = 1
     
@@ -48,9 +54,3 @@ def display_adjacency_matrix(adjacency_matrix):
         for col in headers:
             print(f" {adjacency_matrix[row][col]} |", end="")
         print()
-
-
-
-
-# matrix = adjacency_list_to_matrix("a->b, b->c, c->d, d->b") #Row is source, Col is target
-# display_adjacency_matrix(matrix)
