46d5360df55a9c85fe14843a807f412f473f656d
Snehal <snehal.sobti@gmail.com>
2023-11-11 11:59:24 -0500

    part2.py: Dijkstra Implementation

--- part2.py
+++ part2.py
@@ -16,6 +16,33 @@ def parse_input_string(matrix, input_string):
         
         matrix[source][target] = cost
 
+def dijkstra_algorithm(matrix, source):
+    num_intersections = len(matrix)
+    lowest_costs = [float('inf')] * num_intersections
+    lowest_costs[source] = 0
+    visited_inter = [False] * num_intersections
+    prev_inter = [-1] * num_intersections
 
-def find_optimal_path(matrix, start_intersection, end_intersection, input_string):
+    for _ in range(num_intersections - 1):
+        curr_source = -1
+
+        for inter in range(num_intersections):
+            if not visited_inter[inter]:
+                if (curr_source == -1 or lowest_costs[inter] < lowest_costs[curr_source]):
+                    curr_source = inter
+
+        visited_inter[curr_source] = True
+
+        for curr_target in range(num_intersections):
+            if matrix[curr_source][curr_target] != 0 and not visited_inter[curr_target]:
+                new_distance = lowest_costs[curr_source] + matrix[curr_source][curr_target]
+                if new_distance < lowest_costs[curr_target]:
+                    lowest_costs[curr_target] = min(lowest_costs[curr_target], new_distance)
+                    prev_inter[curr_target] = curr_source
+                
+    return lowest_costs, prev_inter
+
+def visited_intersectionsd_optimal_path(matrix, start_inter, end_inter, input_string):
     parse_input_string(matrix, input_string)
+    lowest_costs, prev_inter = dijkstra_algorithm(matrix, start_inter)
+    
