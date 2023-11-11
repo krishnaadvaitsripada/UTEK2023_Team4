ab32cd71e6081dfe78d3fe812c60cf342ada73ea
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-11-11 11:25:35 -0500

    Complete function to convert input (adj_list) to matrix

--- part1.py
+++ part1.py
@@ -4,18 +4,31 @@ Graph Construction
 This part takes an input of intersections, and organizes it an adjacency matrix fashion
 """
 
-from random import sample
 
 
 def adjacency_list_to_matrix(adj_list):
     adj_list = adj_list.split(',')
     intersections = []
+    intersection_pairs = []
     for intersection_pair in adj_list:
-        for intersection in intersection_pair.split("->"):
+        intersection_pair = intersection_pair.split("->")
+        intersection_pairs.append(intersection_pair)
+        for intersection in intersection_pair:
             intersections.append(intersection.strip())
     
     intersections = set(intersections) # Remove duplicates
-    print(intersections)
+    matrix = {}
+    for intersection_i in intersections:
+        matrix[intersection_i] = {}
+    
+    for intersection_pair in intersection_pairs: 
+        matrix[intersection_pair[0].strip()][intersection_pair[1].strip()] = 1
+    
+
+    return matrix
+
+
+    
 
 
 
